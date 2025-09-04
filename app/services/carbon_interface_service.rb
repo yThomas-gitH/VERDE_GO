b8577d6a285267e9d5a2e1044fe62fb7e723a9b5
Yann Thomas <yannthomas1995@gmail.com>
2025-09-04 16:49:02 +0200

    Climatiq service setup

--- app/services/carbon_interface_service.rb
+++ app/services/carbon_interface_service.rb
@@ -1,6 +1,6 @@
 class CarbonInterfaceService
   include HTTParty
-  base_uri "https://www.carboninterface.com/api/v1/estimates"
+  base_uri "https://www.carboninterface.com/api/v1"
 
   def initialize
     @api_key = ENV["CARBON_INTERFACE_API_KEY"]
@@ -8,4 +8,61 @@ class CarbonInterfaceService
       'Authorization' => "Bearer #{@api_key}",
       'Content-Type' => "application/json"
     }
+  end
+
+  def calculate_transport_emissions(transport_method:, distance_km:, passengers: 1)
+    body = {
+      type: 'vehicle',
+      distance_unit: 'km',
+      distance_value: distance_km,
+      vehicle_model_id: get_vehicle_model_id(transport_method),
+      passengers: passengers
+    }
+
+    response = self.class.post('/estimates', {
+      body: body.to_json,
+      headers: @headers
+    })
+
+    if response.success?
+      data = response.parsed_response['data']['attributes']
+
+      {
+        carbon_kg: data['carbon_kg'],
+        factor_kg_per_km: data['carbon_kg'] / distance_km,
+        uncertainty_percentage: 10
+      }
+    else
+      raise "Carbon API Error: #{response.body}"
+    end
+  end
+
+  private
+    
+    def get_vehicle_model_id(transport_method)
+      # These would be actual model IDs from the CarbonInterface API
+      vehicle_mapping = {
+        'car' => 'a4d0c965-a1eb-4650-bb4c-3d5d7b228c06', # Average car
+        'bus' => 'b2f4c123-b9eb-4650-cc4c-4e6e8c229d07', # City bus
+        'train' => 'c5f5d234-c0fc-4750-dd5d-5f7f9d330e08', # Passenger train
+        'bicycle' => nil, # Zero emissions
+        'walking' => nil, # Zero emissions
+        'taxi' => 'a4d0c965-a1eb-4650-bb4c-3d5d7b228c06' # Same as car
+      }
+      
+      # Return zero emissions for walking/cycling
+      return handle_zero_emissions(transport_method) if vehicle_mapping[transport_method].nil?
+      
+      vehicle_mapping[transport_method]
+    end
+    
+    def handle_zero_emissions(transport_method)
+      # For walking/cycling, return zero emissions
+      if ['walking', 'bicycle'].include?(transport_method)
+        return :zero_emissions
+      end
+      
+      # Fallback to average car
+      'a4d0c965-a1eb-4650-bb4c-3d5d7b228c06'
+    end
   end
---
+++ app/services/climatiq_service.rb
@@ -0,0 +1,63 @@
+class ClimatiqService
+  include HTTParty
+  base_uri "https://api.climatiq.io"
+
+  def initialize
+    @api_key = ENV["CLIMATIQ_API_KEY"]
+    @headers = {
+      'Authorization' => "Bearer #{@api_key}",
+      'Content-Type' => "application/json"
+    }
+  end
+
+  def calculate_transport_emissions(transport_method:, distance_km:)
+    factor = get_emission_factor(transport_method)
+
+    # Cas spécial zéro émission
+    return { carbon_kg: 0, factor_kg_per_km: 0, uncertainty_percentage: 0 } if factor.nil?
+
+    body = {
+      emission_factor: {
+        activity_id: factor,
+        data_version: "25.25"
+      },
+      parameters: {
+        distance: distance_km,
+        distance_unit: "km",
+      }
+    }
+
+    response = self.class.post("/estimate", {
+      body: body.to_json,
+      headers: @headers
+    })
+
+    if response.success?
+      data = response.parsed_response['co2e']
+
+      {
+        carbon_kg: data,
+        factor_kg_per_km: data / distance_km,
+        uncertainty_percentage: 5 # Climatiq fournit souvent des valeurs fiables
+      }
+    else
+      raise "Climatiq API Error: #{response.body}"
+    end
+  end
+
+  private
+
+  def get_emission_factor(transport_method)
+    # Documentation : https://www.climatiq.io/docs/api-reference
+    mapping = {
+      'car' => 'passenger_vehicle-vehicle_type_car-fuel_source_na-engine_size_na-vehicle_age_na-vehicle_weight_na',
+      'bus' => 'passenger_vehicle-vehicle_type_bus-fuel_source_na-engine_size_na-vehicle_age_na-vehicle_weight_na',
+      'train' => 'passenger_vehicle-vehicle_type_train-fuel_source_na-engine_size_na-vehicle_age_na-vehicle_weight_na',
+      'flight'  => 'passenger_flight-route_type_na-aircraft_type_na-distance_short_medium_haul_lt_1000km-class_na-rf_included-distance_uplift_na',
+      'bicycle' => nil,
+      'walking' => nil
+    }
+
+    mapping[transport_method]
+  end
+end
